93b6babaa27cbe006405e69e59b209ad7d344645
I <ibnutsalisassalam@gmail.com>
2023-11-15 09:24:58 +0700

    init

---
+++ __manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name"          :   "Modul App Kelompok 5",
+    "author"        :   ["Ibnu","Khosyi","Helmi","Dila"],
+    "version"       :   "1.0",
+    "summary"       :   "Ini adalah custom modul odoo aplikasi kelompok 5",
+    "depends"       :   []
+}
